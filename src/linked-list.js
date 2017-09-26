85ab862f358b7e491cf78568e4a3a4300d1e4302
gudvinga <gudvinga@gmail.com>
2017-09-26 09:50:51 +0300

    First commit

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,37 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(this.data),
+            curentNode = this.head;
+            if (!curentNode) {
+                this.tail = node;
+                this.length++;
+                return(node);
+            }
+            else {
+                while(curentNode) {
+                    curentNode = curentNode.next;
+                }
+                curentNode.next = node;
+                this.length++;
+                return(node);
+            }
+    }
 
-    head() {}
+    head() {
+        return this.head.data
+    }
 
-    tail() {}
+    tail() {
+        return this.tail.data;
+    }
 
     at(index) {}
 
@@ -23,5 +47,5 @@ class LinkedList {
 
     indexOf(data) {}
 }
-
+console.log(LinkedList.head);
 module.exports = LinkedList;
